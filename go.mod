d7dba9bb570484fc17556b379634f4218a23c021
elmm-programing <86208465+elmm-programing@users.noreply.github.com>
2023-08-26 17:10:49 -0400

    Boilerplate for http server complete

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/elmm-programing/rssagg
 
 go 1.19
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
 
-func main()  {
-  fmt.Println("Hello World")
-  
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+  v1router := chi.NewRouter()
+  v1router.Get("/healthz", handlerReadiness)
+  v1router.Get("/error", handleErr)
+  router.Mount("/v1", v1router)
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server starting on port %v", port)
+	error := srv.ListenAndServe()
+	if error != nil {
+		log.Fatal(error)
+	}
 }
